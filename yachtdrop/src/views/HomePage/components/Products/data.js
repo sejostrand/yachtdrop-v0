8030360e835edbd10f1fcf4a1f822b6504df0edc
Nicolascanala <nicolas.canala@gmail.com>
2021-03-11 17:19:36 +0100

    finally

---
+++ yachtdrop/src/views/HomePage/components/Products/data.js
@@ -0,0 +1,27 @@
+import product1 from '../../img/cocktail.png';
+import product2 from '../../img/time-icon.jpg';
+import product3 from '../../img/truck-icon.png';
+
+export const productData = [
+  {
+    img: product1,
+    alt: 'Drinks',
+    name: 'Choose products online',
+    desc:
+      'Champagne, Wine, Spirits, Cider, Water... You name it, we have it.'
+  },
+  {
+    img: product2,
+    alt: 'Time',
+    name: 'Pick location and time of delivery',
+    desc:
+      'We can deliver wherever you are. A marina, a mooring... Anywhere!'
+  },
+  {
+    img: product3,
+    alt: 'Delivery',
+    name: 'Await your yachtdrop',
+    desc:
+      'Sit back and relax. We will take it from here.'
+  }
+];
