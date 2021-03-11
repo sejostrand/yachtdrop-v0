d5ef6187b1f8d4ea8b88212547111b478c26105f
Sebastian <sejostrand@gmail.com>
2021-03-11 18:06:34 +0000

    update

--- yachtdrop/src/App.js
+++ yachtdrop/src/App.js
@@ -3,12 +3,12 @@ import React from 'react';
 //import GlobalStyles from './styles/global';
 
 import ShopPage from './views/ShopPage/ShopPage';
-import HomePage from './views/HomePage/HomePage'
+import HomePage from './views/HomePage/HomePage';
 
 function App() {
   return (
     <>
-      <HomePage />
+      <ShopPage />
     </>
   );
 }
