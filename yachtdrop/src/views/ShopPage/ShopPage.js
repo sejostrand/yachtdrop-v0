9041976e48dde84f6c90143be686a564d29bd472
Nicolascanala <nicolas.canala@gmail.com>
2021-03-11 12:00:53 +0100

    Added the footer

--- yachtdrop/src/views/ShopPage/ShopPage.js
+++ yachtdrop/src/views/ShopPage/ShopPage.js
@@ -12,6 +12,9 @@ import BodyWrapper from '../../objects/BodyWrapper.js';
 import BodyDiv from '../../objects/BodyDiv.js';
 import SortBy from '../../components/SortBy/SortBy.js';
 
+// Import The footer
+import App from '../HomePage/components/Footer/App'
+
 const StyledShopPage = styled.div``;
 
 const ShopPage = () => {
@@ -27,7 +30,7 @@ const ShopPage = () => {
           <ProductDisplay />
         </BodyDiv>
       </BodyWrapper>
-      <Footer />
+      <App />
     </StyledShopPage>
   );
 };
