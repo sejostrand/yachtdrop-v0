221f2f49cb44480698be0e6e7b704b6dbb5063a2
Sebastian <sejostrand@gmail.com>
2021-03-25 18:40:50 +0000

    update

--- yachtdrop/src/App.js
+++ yachtdrop/src/App.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 //import GlobalStyles from './styles/global';
 
 import ShopPage from '@views/ShopPage/ShopPage';
 import HomePage from '@views/HomePage/HomePage';
-import AboutUs from '@views/AboutUs/AboutUs'
-import ContactUs from '@views/ContactUs/ContactUs'
+import AboutUs from '@views/AboutUs/AboutUs';
+import ContactUs from '@views/ContactUs/ContactUs';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/homepage">
+        <Route path='/homepage'>
           <HomePage />
         </Route>
-        <Route path="/shoppage">
+        <Route path='/shoppage'>
           <ShopPage />
         </Route>
-        <Route path="/aboutus">
+        <Route path='/aboutus'>
           <AboutUs />
         </Route>
-        <Route path="/contactus">
+        <Route path='/contactus'>
           <ContactUs />
         </Route>
       </Switch>
